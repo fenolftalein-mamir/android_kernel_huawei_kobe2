5b674f8d0d6bf4150c17e018531477dbb364a9f1
R0rt1z2 <me@r0rt1z2.com>
2025-07-09 20:36:30 +0200

    lib/: Import Huawei kernel changes
    * From Kobe2_10.1.0_Code_Opensource.tar.gz
    
    Change-Id: I49a8c5035e94b6f1333b2dd8c266d6751b4de61d
    Signed-off-by: R0rt1z2 <me@r0rt1z2.com>

--- lib/kobject_uevent.c
+++ lib/kobject_uevent.c
@@ -28,6 +28,9 @@
 #include <net/sock.h>
 #include <net/net_namespace.h>
 
+#ifdef CONFIG_HUAWEI_DUBAI
+#include <chipset_common/dubai/dubai.h>
+#endif
 
 u64 uevent_seqnum;
 #ifdef CONFIG_UEVENT_HELPER
@@ -502,6 +505,9 @@ int kobject_uevent_env(struct kobject *kobj, enum kobject_action action,
 		} else
 			retval = -ENOMEM;
 	}
+#ifdef CONFIG_HUAWEI_DUBAI
+	dubai_log_uevent(devpath, action);
+#endif
 #endif
 	mutex_unlock(&uevent_sock_mutex);
 
